c2cd1943f4821cdbb1243f3acac72c57af923a6f
Gabriele Pace <gabriele.pace@hotmail.com>
2021-11-09 01:36:41 +0100

    Fixed server.js file

--- server.js
+++ server.js
@@ -10,4 +10,6 @@ app.use('/login', (req, res) => {
     });
 });
 
-app.listen(8080, () => console.log('API is running on http://localhost:8080/login'));
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => console.log(`API is running on http://localhost:${PORT}/login`));
